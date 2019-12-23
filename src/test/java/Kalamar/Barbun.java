a812aee5669029c7e480b671ba6892871fcc9a31
Sefik Omay <sefikomay@gmail.com>
2019-12-23 16:14:38 -0500

    new changes

--- src/test/java/Kalamar/Barbun.java
+++ src/test/java/Kalamar/Barbun.java
@@ -2,12 +2,9 @@ package Kalamar;
 
 public class Barbun {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
 
         System.out.println("hamsi");
-        System.out.println("hjagdfshjagfa");
-
 
     }
 }
---
+++ src/test/java/Practices/LocatorsPractice.java
@@ -0,0 +1,8 @@
+package Practices;
+
+public class LocatorsPractice {
+    public static void main(String[] args) {
+
+
+    }
+}
